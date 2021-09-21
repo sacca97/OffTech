30e3866349e5122bb988a07830f3ede50d18148d
sacca97 <tommaso.sacchetti@gmail.com>
2021-09-21 15:16:23 +0200

    remote command

--- setup.sh
+++ setup.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-rm -f networkStructure.txt
+#rm -f networkStructure.txt
 
-/share/shared/Internetworking/showcabling Sacchetti-IntNetw OffTech > networkStructure.txt
+#/share/shared/Internetworking/showcabling Sacchetti-IntNetw OffTech > networkStructure.txt
 
 #git clone https://github.com/sacca97/OffTech.git
 
-ssh NWworkstation1.Sacchetti-IntNetw.OffTech "sudo mkdir affettati"
+ssh NWworkstation1.Sacchetti-IntNetw.OffTech "sudo sh OffTech/NWworkstation1.sh eth3"
 
-#ssh NWworkstation1.Sacchetti-IntNetw.OffTech "sudo sh OffTech/NWworkstation1.sh ETH ETH"
+ssh NWrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/NWrouter.sh eth3 eth5"
 
-#ssh NWrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/NWrouter.sh ETH ETH"
+ssh ISrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/ISrouter.sh eth3 eth0"
 
-#ssh ISrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/ISrouter.sh ETH ETH"
+ssh SWrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/SWrouter.sh eth2 eth1"
 
-#ssh SWrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/SWrouter.sh ETH ETH"
-
-#ssh SWworkstation1.Sacchetti-IntNetw.OffTech "sudo sh OffTech/SWworkstation.sh ETH ETH"
+ssh SWworkstation1.Sacchetti-IntNetw.OffTech "sudo sh OffTech/SWworkstation.sh eth0"
 
 
 
