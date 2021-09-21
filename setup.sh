c42993f9b2ac0fcc2bf0653fe00990f13ddaefd4
sacca97 <tommaso.sacchetti@gmail.com>
2021-09-21 15:18:23 +0200

    remote command

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@
 
 #git clone https://github.com/sacca97/OffTech.git
 
-ssh NWworkstation1.Sacchetti-IntNetw.OffTech "sudo sh OffTech/NWworkstation1.sh eth3"
+ssh NWworkstation1.Sacchetti-IntNetw.OffTech "sudo sh OffTech/NWworkstation1.sh eth3 && exit && exit"
 
-ssh NWrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/NWrouter.sh eth3 eth5"
+ssh NWrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/NWrouter.sh eth3 eth5 && exit && exit"
 
-ssh ISrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/ISrouter.sh eth3 eth0"
+ssh ISrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/ISrouter.sh eth3 eth0&& exit && exit"
 
-ssh SWrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/SWrouter.sh eth2 eth1"
+ssh SWrouter.Sacchetti-IntNetw.OffTech "sudo sh OffTech/SWrouter.sh eth2 eth1 && exit && exit"
 
-ssh SWworkstation1.Sacchetti-IntNetw.OffTech "sudo sh OffTech/SWworkstation.sh eth0"
+ssh SWworkstation1.Sacchetti-IntNetw.OffTech "sudo sh OffTech/SWworkstation.sh eth0 && exit && exit"
 
 
 
