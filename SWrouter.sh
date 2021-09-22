9837496440f471f7ddabad8cced78c5772ae0467
sacca97 <tommaso.sacchetti@gmail.com>
2021-09-22 11:22:36 +0200

    finalizing python script

--- SWrouter.sh
+++ SWrouter.sh
@@ -7,5 +7,5 @@ route add -net 2.4.6.8 netmask 255.255.255.248 gw 3.5.7.17
 route add -net 172.16.16.0 netmask 255.255.255.248 gw 3.5.7.17
 
 iptables -t nat -A POSTROUTING -o $1 -s 10.0.0.0/29 -j SNAT --to 3.5.7.18
-iptables -t nat -A PREROUTING -i $2 -d 3.5.7.18/32 -p tcp --dport 80 -j DNAT --to 10.0.0.2
+iptables -t nat -A PREROUTING -i $1 -d 3.5.7.18/32 -p tcp --dport 80 -j DNAT --to 10.0.0.2
 
--- readstring.py
+++ readstring.py
@@ -1,10 +1,24 @@
-
+import subprocess
 
 lines = open ("topology.txt", "r").readlines()
-res = []
 
+res = [str.strip().split("<- is \"wired\" to ->") for str in lines]
 
-for str in lines:
-    res.append(str.strip().split("<- is \"wired\" to ->"))
+iFaceList = {}
 
-print(res)
+for s in res:
+    for x in s:
+        t = x.strip().split(" ")
+        if t[0] in iFaceList:
+            iFaceList[t[0]].append(t[1])
+        else:
+            iFaceList[t[0]] = [t[1]]
+
+for iFace in iFaceList:
+    firstCommand = "ssh {}.Sacchetti-IntNetw.OffTech".format(iFace) 
+    if len(iFaceList[iFace]) == 1:
+        secondCommand = "sudo sh OffTech/NWworkstation1.sh {}".format(iFaceList.get(iFace)[0])
+    else:
+        secondCommand = "sudo sh OffTech/NWworkstation1.sh {} {}".format(iFaceList.get(iFace)[0], iFaceList.get(iFace)[1])
+    print(secondCommand)
+    #subprocess.call([firstCommand,secondCommand])
