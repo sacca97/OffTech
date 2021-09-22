4f6a5adef4ab301a700f4bcb07c3319d49e170c9
sacca97 <tommaso.sacchetti@gmail.com>
2021-09-22 12:20:14 +0200

    should work

--- readstring.py
+++ readstring.py
@@ -21,10 +21,9 @@ for s in res:
 
 
 for iFace in iFaceList:
-    firstCommand = "ssh {}.Sacchetti-IntNetw.OffTech".format(iFace) 
     if len(iFaceList[iFace]) == 1:
-        secondCommand = "\"sudo sh OffTech/{}.sh {}\"".format(iFace, iFaceList.get(iFace)[0])
+        secondCommand = "{}.Sacchetti-IntNetw.OffTech \"sudo sh OffTech/{}.sh {}\"".format(iFace, iFace, iFaceList.get(iFace)[0])
     else:
-        secondCommand = "\"sudo sh OffTech/{}.sh {} {}\"".format(iFace, iFaceList.get(iFace)[0], iFaceList.get(iFace)[1])
-    print(firstCommand +" " +secondCommand)
-    subprocess.call([firstCommand,secondCommand])
+        secondCommand = "{}.Sacchetti-IntNetw.OffTech \"sudo sh OffTech/{}.sh {} {}\"".format(iFace, iFaceList.get(iFace)[0], iFaceList.get(iFace)[1])
+    print("ssh " +secondCommand)
+    subprocess.call(["ssh",secondCommand])
